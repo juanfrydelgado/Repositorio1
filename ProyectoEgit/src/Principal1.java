ac9212a47f10cc156f698bf02534c5646c74993d
Administrador <Administrador@JUANFRY>
2015-12-09 12:13:03 +0100

    midif

---
+++ ProyectoEgit/src/Principal1.java
@@ -0,0 +1,17 @@
+
+public class Principal1 {
+
+	
+	public static int suma(int a, int b) {
+		 int c;
+		 c = a + b;
+		 return c;
+		 }
+	public static int resta(int a, int b) {
+		 int c;
+		 c = a - b;
+		 return c;
+		 } 
+
+
+}
